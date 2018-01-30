ccdeed74639f3dc0d927d1d79744c5174b22a177
Netanel Basal <netanel7799@gmail.com>
2018-01-30 22:58:45 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { FilterQuery } from './todos/filter/filter.query';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: "./app.component.html",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
